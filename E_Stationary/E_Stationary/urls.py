11d5d1036d97ffd2b353c5778455d5e66a7a5fad
Abrar Shaik <78590172+devShaik010@users.noreply.github.com>
2022-10-09 23:55:48 +0530

    Delete E_Stationary directory

--- E_Stationary/E_Stationary/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path , include
-from Ink_Spot import urls
-
-urlpatterns = [
-    path('',include('Ink_Spot.urls')),
-    path('admin/', admin.site.urls),
-]
--- E_Stationary/Ink_Spot/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .product import Product
-from .categories import Categories
