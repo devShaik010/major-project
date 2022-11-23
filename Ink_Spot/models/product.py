557cbd4d63db6022e58e103a84f98061e247ab36
Abrar Shaik <78590172+devShaik010@users.noreply.github.com>
2022-11-23 23:16:12 +0530

    Add files via upload

---
+++ Ink_Spot/models/product.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .categories import Categories
+
+# this is for product card items 
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    discount = models.CharField(max_length=10)
+    price = models.IntegerField(default=0)
+    desc = models.CharField(max_length=40)
+    image = models.ImageField(upload_to="Uploads\product")
+    categories = models.ForeignKey(Categories,on_delete=models.CASCADE, default=1)
+
+   #storing data in function 
+    @staticmethod
+    def get_all():
+        return Product.objects.all()
