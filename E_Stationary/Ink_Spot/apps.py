c67a1868d088347eb2681414749d275e8ea3d065
Abrar Shaik <78590172+devShaik010@users.noreply.github.com>
2022-11-23 23:10:11 +0530

    Delete E_Stationary directory

--- E_Stationary/Ink_Spot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InkSpotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Ink_Spot'
--- E_Stationary/Ink_Spot/models/b_catogeries.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class B_categories(models.Model):
-    name = models.CharField(max_length=20)
