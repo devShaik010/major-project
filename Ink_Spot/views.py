48a2216ee7260459ec31e6bdce85154b1e11477b
Abrar Shaik <78590172+devShaik010@users.noreply.github.com>
2022-12-31 11:02:36 +0530

    new code
    new code deploy

--- Ink_Spot/views.py
+++ Ink_Spot/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , HttpResponse 
+
+# Import packages from Models
 from .models.product import Product
 from .models.categories import Categories
 from .models.book import Book
 from .models.b_catogeries import B_categories
 
+
+
 # Create your views here.
-
-
-
 def index(request):
     return render(request, 'index.html')
 
@@ -19,29 +19,44 @@ def login(request):
     return render(request, 'login.html')
 
 
-# for product _ stationary items
-
-cat = Categories.all_catogaries()
-
-product = Product.get_all()
-data = {} 
-data['product'] = product
-data['catogereis'] = cat
-
 
 
 # for Books
 
-bcat = B_categories.all_catogaries()
-
 book = Book.get_all()
-bdata = {} 
-bdata['book'] = book
-bdata['b_catogereis'] = bcat
-
-def cat(request):
-    return render(request, 'product.html',data)
 
 def book(request):
-    return render(request, 'book.html',bdata)   
+    book = None
+    bcat = B_categories.all_catogaries()
+    categoryID = request.GET.get('categories')
+    if categoryID:
+        book = Book.get_all_filter(categoryID )
+    else:
+        book = Book.get_all()
+
+    bdata = {} 
+    bdata['book'] = book
+    bdata['b_catogereis'] = bcat
+
+    return render(request, 'book.html',bdata)
+
+  # for product _ stationary items
+def product(request):
+    product = None
+    cat = Categories.all_catogaries()
+    categoryID = request.GET.get('categories')
+    print(categoryID)
+    
+    if categoryID:
+        product = Product.get_all_filter(categoryID) 
+    else:
+        product = Product.get_all() 
+
+
+    data = {} 
+    data['product'] = product
+    data['catogereis'] = cat
+    return render(request, 'product.html',data)
+
+   
      
